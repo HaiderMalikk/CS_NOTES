22a3337a439ede309f2ce22a761827bcd28c0517
HaiderMalikk <haidermalik662@gmail.com>
2024-11-30 15:58:57 -0500

    tail vs non tail recurtion

--- Python/Algorithms/Recursion.py
+++ Python/Algorithms/Recursion.py
@@ -142,3 +142,100 @@ def fibonacci(n):
 # test
 fibonacci(10) # output: 55
 # NOTE: fib(n-1) is called before fib(n-2) in every recursive call because fib(n-1) is being called before fib(n-2) once fib(n-1) returns the result to its caller then the caller calls fib(n-2) to be added.
+
+# ! tail recursion
+""" 
+In tail recursion, the recursive call is the last operation in the function. This means that no further computation is required after the recursive call returns.
+
+Characteristics:
+The result of the current function call is directly the result of the recursive call.
+It is memory-efficient if the compiler/interpreter optimizes it using tail call optimization (TCO), as it does not need to keep track of previous function states.
+
+- in short we pass the result of the current function call to the next function call as an argument
+hence when we reach the end (base case) we return the result of the last function call as we have continuesly passed each 
+result to the next function call the last call i.e the one that reached the base case already has the end result and hence we return the final result.
+
+NOTE: when we goto the next recursive function call we preform the computation we then pass the result to the next function call and so on
+      until we reach the base case. then when we return from each function call we return the result of the last function call and no further computation is required.
+      so when we move forward we preform the computation and give it to the next function call as a argument 
+      and when we move backward we return the result of the last function call and preform no computation. 
+"""
+# EX : factorial
+# accumulator is the result of the previous function call, its default value is 1 as factorial(0) = 1 
+def factorial_tail(n, accumulator=1): 
+    if n == 0:
+        return accumulator  # Base case once we reach n = 0 we return the accumulator which is the result of the factorial on n
+    # in each recursive function call we calculate the factorial so far which is n * accumulator we calculate the factorial now
+    # and pass it to the next function call as an argument along with n = n - 1
+    return factorial_tail(n - 1, accumulator * n)  # Recursive case 
+
+print(factorial_tail(5))  # Output: 120
+
+""" 
+step by step
+Initial Call: factorial_tail(5, 1)
+Recursive Call: factorial_tail(5 - 1 = 4, 1 * 5 = 5) # this result is passed to the next function call
+Recursive Call: factorial_tail(4 - 1 = 3, 5 * 4 = 20) 
+Recursive Call: factorial_tail(3 - 1 = 2, 20 * 3 = 60)
+Recursive Call: factorial_tail(2 - 1 = 1, 60 * 2 = 120)
+Recursive Call: factorial_tail(1 - 1 = 0, 120 * 1 = 120)
+Base Case n = 0 for function call factorial_tail(0, 120); returns 120 to caller
+
+factorial_tail(0, 120) returns 120 to its caller of factorial_tail(1, 120) 
+factorial_tail(1, 120) returns 120 to its caller of factorial_tail(2, 60)
+factorial_tail(2, 60) returns 120 to its caller of factorial_tail(3 , 20)
+factorial_tail(3, 20) returns 120 to its caller of factorial_tail(4 , 5)
+factorial_tail(4, 5) returns 120 to its caller of factorial_tail(5 , 1) 
+factorial_tail(5, 1) is the initial call and  returns 120 to its caller which is factorial(5) in the print statement
+
+- notice how when the calculated factorial in the last call is returned to the caller no calculation is performed by the caller as we 
+  did that when we were going to the next function call so all we do when returning from the last call is return the result to the caller and so on
+  until we return to user.
+"""
+
+# ! Non-Tail Recursion
+""" 
+In non-tail recursion, the recursive call is not the last operation in the function. After the recursive call returns, some operations still need to be performed.
+
+Characteristics:
+Requires additional memory to store intermediate states, as the function cannot be completed until all recursive calls return.
+Typically used in problems where results need to be combined after recursive calls.
+
+NOTE: after we reach the base case we return to the caller and then we preform the computation we then pass the result to the previous function call
+      and so on until we reach the first call, but as we are going backwards we are performing some computation in each function call
+      so when we were going forward we did not preform any computation.
+"""
+# the only argument to the function is n
+def factorial_non_tail(n): 
+    # if n is 0 we return 1 the base case as 0! = 1
+    if n == 0: 
+        return 1  # Base case
+    
+    # in each recursive function call we pass n * n-1 to the next function call but since n needs fact(n-1) to preform the computation
+    # we must get the result of fact(n-1) before passing multiplying it to n this means we only pass the n-1 to the next function call
+    # and when we reach reach the base case and return the result to the caller we preform the computation then return the result
+    # to the caller and preform the computation again with this new result and so on until we reach the first call
+    return n * factorial_non_tail(n - 1)  # Recursive call is not the last operation
+
+print(factorial_non_tail(5))  # Output: 120
+
+""" 
+step by step
+Initial Call: factorial_non_tail(5)
+Recursive Call: factorial_non_tail(5 - 1 = 4)
+Recursive Call: factorial_non_tail(4 - 1 = 3)
+Recursive Call: factorial_non_tail(3 - 1 = 2)
+Recursive Call: factorial_non_tail(2 - 1 = 1)
+Recursive Call: factorial_non_tail(1 - 1 = 0)
+Base Case n = 0 for function call factorial_non_tail(0); returns 1 to caller
+
+factorial_non_tail(0) returns 1 to its caller of factorial_non_tail(1) and it dose the computation 1(n for fact(1)) * 1(result of fact(0) = 1 we returned this to the caller fact(2)
+factorial_non_tail(1) returns 1 to its caller of factorial_non_tail(2) and it dose the computation 2 * 1 = 2
+factorial_non_tail(2) returns 2 to its caller of factorial_non_tail(3) and it dose the computation 3 * 2 = 6
+factorial_non_tail(3) returns 6 to its caller of factorial_non_tail(4) and it dose the computation 4 * 6 = 24
+factorial_non_tail(4) returns 24 to its caller of factorial_non_tail(5) and it dose the computation 5 * 24 = 120
+
+- notice how we dont preform any computation when calling the next function we simply keep passing the arguments until we reach the base case
+  then when we return this result to the caller we preform the computation and pass the result to the previous function call and so on until we reach the first call
+"""
+
