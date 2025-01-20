b7cdd1430490adea268d4314956991533ced5683
HaiderMalikk <haidermalik662@gmail.com>
2025-01-20 16:29:49 -0500

    ReactJS

--- Syntax/Syntax.js
+++ Syntax/Syntax.js
@@ -1,6 +1,7 @@
 // Javascript intro and syntax
 // JS6 syntax and features
-// ! the ';'  is not needed in js but is good practice
+// !!! the ';'  is not needed in js but is good practice
+
 
 console.log("start") // print to console
 
@@ -27,19 +28,24 @@ funcname(1);
 
 // * conditionals if will run always no matter if a privious if was true else if will run if previous if was false and else runs if all fails
 // * and and or is same as java
-// * functions can be nested you can return functions and tehy can have default parameters
-if (num = 10){
-    console.log("num is 10");
-}
-else if  (num > 10 ||  num < 10 &&  num != 10){
-    console.log("num is greater than 10");
-    let str = "or"
-    let number = 1
-    return number || str; // if number DNE then return str
-}
-else{
-    console.log("num is less than 10");
-}
+// here we use a arrow function to return a value
+const processNumber = (num) => {
+    if (num === 10) {
+      console.log("num is 10");
+    } else if (num > 10 || (num < 10 && num !== 10)) {
+      console.log("num is greater than 10");
+      let str = "or";
+      let number = 1;
+      return number || str; // Returns `number` if it exists; otherwise, `str`
+    } else {
+      console.log("num is less than 10");
+    }
+};
+  
+// Example 
+const result = processNumber(11);
+console.log("Returned value:", result);
+  
 
 // * switch case (always cehcks equality and type ie ===)
 switch (num) {
@@ -67,10 +73,16 @@ var1 = null;
 var2 = 88;
 console.log(var1 ?? var2); // if x DNE then return y
 
-// * arrow functions 
-// format is function name = (arg1, arg2) => {return arg1 + arg2}   // can also specify return type and function type
+// * arrow functions, used to create functions in a more concise way than normal functions
+// format is function name = (arg1, arg2) => {return 'code'}   // can also specify return type and function type
 add = (a, b) => {return a + b}
 console.log(add(1, 2));
+// ex with calling function from a function using arrow function 
+// here we pass in no parameters and call the add function in the function code meaning it will add 2 and 2 always
+funcname2 = () => {
+    return add(2, 2)
+}
+console.log(funcname2());
 
 // * equality operators
 // '=' just assigns '==' checks equality only '===' checks equality and type
@@ -196,3 +208,71 @@ console.log(location.position?.lat); // if position DNE it will return undefined
 console.log(location.positionFormatted?.lat); // positionFormatted DNE so it will return undefined
 // error checking with ??
 console.log(location.positionFormatted ?? "LocationFormatted DNE"); // if positionFormatted DNE then return "Location DNE"
+
+
+// Async, Await, Promises
+// a async function is a function that returns a promise
+// a promise is an object that represents the eventual completion or failure of an asynchronous operation
+// await is used to wait for a promise i.e a async function to resolve 
+
+// asyncrnous means that the code will not wait for the function to finish before moving on to the next line of code 
+// it will run in the background parallel to the rest of the code
+
+// Create a Promise, here resolve and reject are functions that are passed to the Promise and are responsible for resolving or rejecting the promise
+// no arguments are passed to the resolve or reject functions
+// its a arrow function not a normal function because it returns a promise
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const success = true;
+        if (success) {
+          resolve("Data fetched successfully!");
+        } else {
+          reject("Error fetching data.");
+        }
+      }, 1000); // Simulate a delay of 1 second
+    });
+};
+  
+// Using the Promise use .then to handle resolve and .catch to handle reject from the data
+fetchData()
+    .then((data) => console.log(data)) // Handles resolve
+    .catch((error) => console.error(error)); // Handles reject
+
+// Using async/await to handle the Promise
+const getData = async () => {
+    try {
+      const data = await fetchData(); // Waits for the Promise to resolve
+      console.log(data);
+    } catch (error) {
+      console.error(error); // Handles rejection
+    }
+};
+  
+getData();
+
+// handling multiple promises, we dont have a reject here beacuse our promises will always resolve in this example, but the catch block will still run if any promise fails
+const fetchData1 = () => {
+    return new Promise((resolve) => setTimeout(() => resolve("Data 1"), 1000));
+  };
+  
+  const fetchData2 = () => {
+    return new Promise((resolve) => setTimeout(() => resolve("Data 2"), 2000));
+  };
+  
+  const fetchData3 = () => {
+    return new Promise((resolve) => setTimeout(() => resolve("Data 3"), 1500));
+  };
+  
+  // Using Promise.all
+const fetchAllData = async () => {
+    try {
+      const results = await Promise.all([fetchData1(), fetchData2(), fetchData3()]);
+      console.log("All data fetched:", results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+};
+  
+fetchAllData();
+
